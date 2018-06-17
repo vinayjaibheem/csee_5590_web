8535d6e1994ef531773a20f06aa62b4bbbe0eae0
vinayjaibheem <vinayjois12@gmail.com>
2018-06-16 19:49:14 -0500

    Assignment

---
+++ ICP5/Foursquare API/Source/app.js
@@ -0,0 +1,45 @@
+// 'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [])
+
+    .controller('View1Ctrl', function ($scope, $http) {
+        $scope.venueList = new Array();
+        $scope.mostRecentReview;
+        $scope.getVenues = function () {
+            var placeEntered = document.getElementById("txt_placeName").value;
+            var searchQuery = document.getElementById("txt_searchFilter").value;
+            if (placeEntered != null && placeEntered != "" && searchQuery != null && searchQuery != "") {
+                //This is the API that gives the list of venues based on the place and search query.
+                var handler = $http.get("https://api.foursquare.com/v2/venues/search" +
+                    "?client_id=IMBQHWSRZV4KU33RA415LBPUOGTQCBX2MT03HEU0ZNJJWVP2" +
+                    "&client_secret=WEZCA1CKBM1LO5WLLRRY3FLEY0V3LX4PO34LGZYHVMJ4SAZU" +
+                    "&v=20160215&limit=5" +
+                    "&near=" + placeEntered +
+                    "&query=" + searchQuery);
+
+                handler.success(function (data) {
+
+                    $scope.venuList=new Array();
+                    if (data != null && data.response != null && data.response.venues != undefined && data.response.venues != null) {
+                        // Tie an array named "venueList" to the scope which is an array of objects.
+                        // Each object should have key value pairs where the keys are "name", "id" , "location" and values are their corresponding values from the response
+                        // Marks will be distributed between logic, implementation and UI
+
+                        for (var i = 0; i < data.response.venues.length; i++) {
+                            $scope.venueList[i] = {
+                                "name": data.response.venues[i].name,
+                                "id": data.response.venues[i].id,
+                                "location": data.response.venues[i].location,
+                                "contact":data.response.venues[i].contact
+                            };
+                        }
+
+                    }
+                })
+                handler.error(function (data) {
+                    alert("There was some error processing your request. Please try after some time.");
+                });
+            }
+        }
+    });
